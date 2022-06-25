9e3b19cd890e18dc53d54bb1b8c1cfb49cbd23f8
TailssTF <tailss5@mail.ru>
2022-06-25 19:49:37 +0700

    commit

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -2,51 +2,55 @@ import '../styles/Menu.css'
 
 function Menu() {
       return(
-            <nav className='side-menu'>
+            <div>
                   <input type='checkbox' id='menu-toggle' hidden />
-                  <label for='menu-toggle' class='menu-toggle' onclick />
-                  <div className='side-header'>
-                        <img src='/images/Side/logo.svg' className='side-logo'/>
-                        <div className='side-header-menu'>
-                              <a href='#'>О комплексе</a>
-                              <a href='#'>Блог</a>
-                              <a href='#'>FAQ</a>
-                              <a href='#'>Контакты</a>
-                        </div>
-                        <div className='side-login'>
-                              <img src='/images/Side/key.svg' className='side-key'/>    
-                              <a href='#' className='side-login'>ВХОД</a>
-                        </div>
-                  </div>
-                  <div className='side-menu-content'>
-                        <div className='side-social'>
-                              <div className='side-line' />
-                              <div className='side-social-buttons'>
-                                    <button href='#' className='side-social-btn'>
-                                          <img src='/images/Side/facebook.svg' className='side-social-logo' />
-                                    </button>
-                                    <button href='#' className='side-social-btn'>
-                                          <img src='/images/Side/twitter.svg' className='side-social-logo' />
-                                    </button>
-                                    <button href='#' className='side-social-btn'>
-                                          <img src='/images/Side/instagram.svg' className='side-social-logo' />
-                                    </button>
+                  <nav className='side-menu'>
+                        <label for='menu-toggle' class='menu-toggle' onclick />
+                        <div className='side-header'>
+                              <img src='/images/Side/logo.svg' className='side-logo'/>
+                              <div className='side-header-menu'>
+                                    <a href='#'>О комплексе</a>
+                                    <a href='#'>Блог</a>
+                                    <a href='#'>FAQ</a>
+                                    <a href='#'>Контакты</a>
+                              </div>
+                              <div className='side-login'>
+                                    <img src='/images/Side/key.svg' className='side-key'/>    
+                                    <a href='#' className='side-login'>ВХОД</a>
                               </div>
                         </div>
-                        <div className='side-category'>
+                        <div className='side-menu-box'>
+                              <div className='side-social'>
+                                    <div className='side-line' />
+                                    <div className='side-social-buttons'>
+                                          <button href='#' className='side-social-btn'>
+                                                <img src='/images/Side/facebook.svg' className='side-social-logo' />
+                                          </button>
+                                          <button href='#' className='side-social-btn'>
+                                                <img src='/images/Side/twitter.svg' className='side-social-logo' />
+                                          </button>
+                                          <button href='#' className='side-social-btn'>
+                                                <img src='/images/Side/instagram.svg' className='side-social-logo' />
+                                          </button>
+                                    </div>
+                              </div>
+                              <div className='side-menu-main'>
+                                    <div className='side-menu-body'>
+                                          <div className='side-category'>
 
+                                          </div>
+                                          <div className='side-banner'>
+                                                
+                                          </div>
+                                    </div>
+                                    <div className='side-footer'>
+                                          <a href='mail:info@mriyaresort.com' className='side-mail'>info@mriyaresort.com</a>
+                                    </div>
+                              </div>      
                         </div>
-                        <div className='side-banner'>
 
-                        </div>
-                  </div>
-                  <div className='side-footer'>
-                        <a href='mail:info@mriyaresort.com'>info@mriyaresort.com</a>
-                  </div>
-                  <button className='side-menu-pad' onClick={toggleMenu()}>
-                        <img src='/images/Side/Btn/open.png' className='side-menu-btn'/>
-                  </button>
-            </nav>
+                  </nav>
+            </div>
       );
 }
 
